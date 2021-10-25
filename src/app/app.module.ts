a734c861f2c51a7550eea9edd5d7ea96be6fc843
iSrFlashed <jose1216961@hotmail.com>
2021-10-24 21:01:47 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorComponent } from './contador/contador/contador.component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
+import { HeoresModule } from './heroes/herores.module';
+import { ListadoComponent } from './heroes/listado/listado.component';
+import { ContadorModule } from './contador/contador.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeoresModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
